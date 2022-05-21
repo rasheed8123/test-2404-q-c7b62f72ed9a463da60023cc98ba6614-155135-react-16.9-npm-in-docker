9e3c7454bc05e22873800b2f2821d390d821db0d
Avinash K <K3076183@gmail.com>
2022-05-21 10:25:48 +0000

    firstcommit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {useState , useEffect} from "react"
+
 function App() {
+  const [data,setData]= useState([])
+  
+  useEffect(()=>{
+    getdata()
+  },[])
+
+  const getdata = async ()=>{
+      try {
+        let res = await fetch("https://codejudge-question-artifacts-dev.s3.amazonaws.com/q-1710/data.json")
+        let data = await res.json()
+   
+        setData(data.data)
+      } catch (error) {
+         console.log(error)
+      }
+  }
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {data.map((e)=>(
+          <div className='flex'>
+             <div><h1>{e.company}</h1></div>
+             <div className='h3'>  
+               <h3>{e.role}</h3>
+               <h3>{e.technology}</h3>
+               <h3>{e.ctc}LPA</h3>
+             </div>
+          </div>
+        ))}
     </div>
   );
 }
